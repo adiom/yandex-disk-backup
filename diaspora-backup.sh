30771255d93b2b8e69205fdd702dbd8508e29c37
Adiom Timur <adiom@canfly.org>
2015-11-03 02:41:16 +0300

    diaspora backup update

--- diaspora-backup.sh
+++ diaspora-backup.sh
@@ -7,9 +7,10 @@ DUMPNAME="diaspora_production_$DUMPDATE.sql.asc"
 TARNAME="amalgama_$DUMPDATE.tar"
 
 ${MYSQLDUMP} ${MYSQLDUMPOPTS} | gpg --encrypt -r support@canfly.org > ${DUMPNAME}
-tar cvf ${TARNAME} /home/diaspora/amalgama/
-gpg --encrypt -r "support@canfly.org" ${TARNAME}
 
-go.sh ${DUMPNAME}
+tar cvz /home/diaspora/amalgama/| gpg --encrypt -r "support@canfly.org" > ${TARNAME}.gpg
+
+./go.sh ${DUMPNAME}
+./go.sh ${TARNAME}.gpg
 
 #rm -f $(DUMPNAME)
